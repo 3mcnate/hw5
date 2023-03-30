f025afd2ee3da111ae87ff5655672aa6e2b3d9c0
Nate Boxer <nboxer@usc.edu>
2023-03-30 14:15:49 -0700

    wordle written, untested

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,12 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void buildWords(
+    std::string word, 
+    size_t pos, 
+    const std::string& in, 
+    const std::string& floating, 
+    std::set<std::string>& possibleWords);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +26,56 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> possibleWords;
+    std::string emptyWord(in.size(), '-');
 
+    buildWords(emptyWord, 0, in, floating, possibleWords);
+
+    std::set<std::string> solutions;
+    for (std::set<std::string>::iterator it = possibleWords.begin(); it != possibleWords.end(); ++it) {
+        if (dict.find(*it) != dict.end()) {
+            solutions.insert(*it);
+        }
+    }
+    return solutions;
 }
 
 // Define any helper functions here
+void buildWords(
+    std::string word, 
+    size_t pos, 
+    const std::string& in, 
+    const std::string& floating, 
+    std::set<std::string>& possibleWords)
+{
+    // base case: we've built up the whole word.
+    if (pos == word.size()) {
+        // check if the word meets floating point constraints.
+        for (size_t i = 0; i < floating.size(); i++) {
+            bool foundLetter = false;
+            for (size_t j = 0; j < word.size(); j++) {
+                if (floating[i] == word[j]) {
+                    foundLetter = true;
+                    break;
+                }
+            }
+            if (!foundLetter) {
+                return;
+            }
+        }
+        possibleWords.insert(word);
+        return;
+    }
+
+    // add letter to word
+    if (in[pos] != '-') {
+        word[pos] = in[pos];
+        buildWords(word, pos+1, in, floating, possibleWords);
+    }
+    else {
+        for (char c = 'a'; c < 'z'+1; c++) {
+            word[pos] = c;
+            buildWords(word, pos+1, in, floating, possibleWords);
+        }
+    }
+}
